b62b6fd235f0f3c78e33ad776a634a19c8d3e6e2
Heorhii <grishasht@bigmir.net>
2019-04-21 00:04:59 +0300

    Corrected codestyle and some warnings

--- src/main/java/services/FileServices.java
+++ src/main/java/services/FileServices.java
@@ -20,30 +20,29 @@ public class FileFunctions {
 
     private static class SingletonHolder {
         private static final FileFunctions INSTANCE = new FileFunctions();
-
         private SingletonHolder() {
         }
     }
 
-    public void openFile(String filePath) throws IOException {
+    public void openFile (String filePath) throws IOException {
         buffReader = new BufferedReader(new FileReader(filePath));
     }
 
-    public String readFromFile() throws IOException {
+    public String readFromFile () throws IOException {
         String buff = this.buffReader.readLine();
         return buff != null ? buff : "";
     }
 
-    public void closeFile() throws IOException {
+    public void closeFile () throws IOException {
         buffReader.close();
     }
 
-    public void writeInFile(String filePath, List<Song> songs) throws IOException {
-        /*Write in file method*/
+    /*Write in file method*/
+    public void writeInFile (String filePath, List < Song > songs) throws IOException {
         FileWriter fr = new FileWriter(new File(filePath), false);
         BufferedWriter br = new BufferedWriter(fr);
         PrintWriter pr = new PrintWriter(br);
-        for(Song song: songs) {
+        for (Song song : songs) {
             pr.println(song.getName() + "-" + song.getAuthor() + "-" +
                     song.getDuration() + "-" + song.getGenre() + "-");
         }
